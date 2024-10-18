ca960ab5019f7f4c0ac4425790bb72e894832149
_vitalik <79845749+vitalik1609@users.noreply.github.com>
2024-10-18 20:43:35 +0300

    Add files via upload

---
+++ Лаб 3/task1.py
@@ -0,0 +1,18 @@
+# TODO Напишите функцию для поиска индекса товара
+
+def find_item_ind(lst, el):
+    try:
+        return lst.index(el)
+    except:
+        return None
+
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_ind(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
