b640dd6ff9ac0194076feb5d264f10162438109b
_vitalik <79845749+vitalik1609@users.noreply.github.com>
2024-10-21 12:18:51 +0300

    Update task1.py

--- Лаб 1/task1.py
+++ Лаб 1/task1.py
@@ -1,11 +1,6 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-count = len(numbers)
-for i in range(len(numbers)):
-    if not numbers[i]:
-        l = sum(numbers[:i])+sum(numbers[i+1:])
-        numbers[i] = l/count
-
-
+ind = numbers.index(None)
+numbers[ind] = (sum(numbers[:ind])+sum(numbers[ind+1:]))/len(numbers)
 print("Измененный список:", numbers)
