d585ec20db790c7a8689b50f53f52b314cdacd25
_vitalik <79845749+vitalik1609@users.noreply.github.com>
2024-11-06 11:30:45 +0300

    Update task2.py

--- Лаб 3/task2.py
+++ Лаб 3/task2.py
@@ -1,7 +1,7 @@
 # TODO Напишите функцию find_common_participants
 
 def find_common_participants(group1, group2, sep=','):
-        return list(set(group1.split(sep)).intersection(set(group2.split(sep))))
+        return list(set(group1.split(sep)).intersection(group2.split(sep)))
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
